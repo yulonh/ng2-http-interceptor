19576dffe7dd8587925dc6a8a41e91675409aea5
dengyulong <421998192@qq.com>
2016-12-11 19:24:21 +0800

    return false in before method can cancel the request.

--- src/http_interceptor_backend.ts
+++ src/http_interceptor_backend.ts
@@ -1,25 +1,28 @@
 /**
  * Created by yulonh on 2016/11/22.
  */
-import { ConnectionBackend, XHRConnection, XHRBackend } from '@angular/http'
-import { HttpInterceptor } from './http_interceptor';
+import {ConnectionBackend, XHRConnection, XHRBackend} from '@angular/http'
+import {HttpInterceptor} from './http_interceptor';
 
 export class HttpInterceptorBackend implements ConnectionBackend {
-  constructor( private httpInterceptors: HttpInterceptor[], private xhrBackend: XHRBackend) {
-  }
-
-  createConnection(request: any): XHRConnection {
-    let req: any = request, interceptor: HttpInterceptor;
-    for (interceptor of this.httpInterceptors) {
-      req = interceptor.before ? interceptor.before(req) : req;
+    constructor(private httpInterceptors: HttpInterceptor[], private xhrBackend: XHRBackend) {
     }
 
-    let result: XHRConnection = this.xhrBackend.createConnection(req);
+    createConnection(request: any): XHRConnection {
+        let req: any = request, interceptor: HttpInterceptor;
+        for (interceptor of this.httpInterceptors) {
+            req = interceptor.before ? interceptor.before(req) : req;
+            if (!req) {
+                return;
+            }
+        }
 
-    for (interceptor of this.httpInterceptors) {
-      result.response = interceptor.after ? interceptor.after(result.response) : result.response;
+        let result: XHRConnection = this.xhrBackend.createConnection(req);
+
+        for (interceptor of this.httpInterceptors) {
+            result.response = interceptor.after ? interceptor.after(result.response) : result.response;
+        }
+
+        return result;
     }
-
-    return result;
-  }
 }
