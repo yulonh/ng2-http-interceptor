cd5b69cfdb1cb7e581ce09e13169f4bd89a839a7
yulonh <421998192@qq.com>
2017-02-15 04:48:28 +0800

    delete

--- src/app/interceptors/test.interceptor.ts
+++
@@ -1,9 +0,0 @@
-import {HttpInterceptor} from "../../http_interceptor";
-import {Request} from "@angular/http";
-export class TestInterceptor implements HttpInterceptor {
-  before(request: Request) {
-    console.log("TestInterceptor");
-    request.url = '/interceptor';
-    return request;
-  }
-}
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export {HttpInterceptor} from './http_interceptor';
-export {HttpInterceptorModule} from './http_interceptor_module';
-export {HttpInterceptorBackend} from './http_interceptor_backend';
